e7786d25c5bbb0ce1c3bfb104a6dd745d7e412b9
ahri0857 <164606858+ahri0857@users.noreply.github.com>
2024-03-24 16:25:12 +0800

    update 完成生成籃球

---
+++ Assets/scrpit/basketballsystem.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class basketballsystem : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject basketball;
+
+    // 哈摟
+
+    public void SpawnBasketball() 
+    {
+        Instantiate(basketball, transform.position, Quaternion.identity);
+    }
+}
